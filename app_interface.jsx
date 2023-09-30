02622ce31a75aef26de13f7d08d09d9a5af2129d
Sanjay Krishna <144625268+SierraHacks@users.noreply.github.com>
2023-09-30 21:32:09 +0000

    First react.js commit

---
+++ app_interface.jsx
@@ -0,0 +1,10 @@
+import {createRoot} from "react-dom/client"
+
+export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <getUserDetails />
+      </div>
+    );
+  }
