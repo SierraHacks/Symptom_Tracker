ce8b2b88e6fbcb21571fb633b7cd2020195da9e9
noob10293 <moob10293@gmail.com>
2023-09-30 17:27:28 -0700

    Add npm packages

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Symptom_Tracker
 Tracks user symptoms and gives an analysis of most relevant health conditions the user may have.
+
+# installation
+run `npm install` in the terminal
+
+# running
+run `npm run start` in the terminal
--- app_interface.jsx
+++ app_interface.jsx
@@ -1,6 +1,27 @@
 import {createRoot} from "react-dom/client"
 
 export default function MyApp() {
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ animal: animalInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+    } catch(error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
     return (
       <div>
         <h1>Welcome to Symptom Tracker</h1>
